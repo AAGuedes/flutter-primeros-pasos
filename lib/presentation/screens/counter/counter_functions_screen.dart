e881374653ec03c3ec86fef8508848e0e92f88d0
AAGuedes <airam.a.g.85@gmail.com>
2023-08-16 11:02:48 +0100

    Widgets personalizados

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -12,57 +12,62 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter Functions'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh_rounded),
-            onPressed: () {
-              setState(() {
-                clickCounter = 0;
-              });
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$clickCounter',
-              style:
-                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+        appBar: AppBar(
+          title: const Text('Counter Functions'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh_rounded),
+              onPressed: () {
+                setState(() {
+                  clickCounter = 0;
+                });
+              },
             ),
-            Text(
-              (clickCounter == 1) ? 'Click' : 'Clicks',
-              style: const TextStyle(fontSize: 25),
-            )
           ],
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                clickCounter++;
-              });
-            },
-            child: const Icon(Icons.plus_one),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ),
+              Text(
+                (clickCounter == 1) ? 'Click' : 'Clicks',
+                style: const TextStyle(fontSize: 25),
+              )
+            ],
           ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                clickCounter--;
-              });
-            },
-            child: const Icon(Icons.exposure_minus_1),
-          ),
-        ],
-      )
+        ),
+        floatingActionButton: const Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(icon: Icons.refresh_outlined),
+            SizedBox(height: 10),
+            CustomButton(icon: Icons.plus_one_outlined),
+            SizedBox(height: 10),
+            CustomButton(icon: Icons.exposure_minus_1_outlined),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+      },
+      child: Icon(icon),
     );
   }
 }
