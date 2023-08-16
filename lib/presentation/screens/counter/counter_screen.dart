97ec798af59ff0d760a9b0f089cdda3fa9df6c4c
AAGuedes <airam.a.g.85@gmail.com>
2023-08-16 10:30:51 +0100

    Tarea - Cambiar la palabra clicks

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -13,7 +13,11 @@ class _CounterScreenState extends State<CounterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Screen'),
+        title: const Center(
+          child: Text(
+            'Counter Screen'
+          )
+        ),
       ),
       body: Center(
         child: Column(
@@ -24,9 +28,9 @@ class _CounterScreenState extends State<CounterScreen> {
               style:
                   const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            const Text(
-              'Clicks',
-              style: TextStyle(fontSize: 25),
+            Text(
+              (clickCounter == 1) ? 'Click' : 'Clicks',
+              style: const TextStyle(fontSize: 25),
             )
           ],
         ),
